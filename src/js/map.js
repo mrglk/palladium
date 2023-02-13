b4dbb2eb785964ebc7ef70c833fc53ed20c849f0
Мария Романович <mariagalko.info@gmail.com>
2023-02-13 21:31:36 +0800

    Fixed map

--- src/js/map.js
+++ src/js/map.js
@@ -229,6 +229,11 @@ const DUBAI_BOUNDS = {
 export async function initMap() {
   const loader = new Loader('AIzaSyCFVN9lovd3dC-PKoK-7VENM-vhhNUcXS8', {});
   const google = await loader.load();
+  
+  mapPointsData.en.forEach(({img}) => {
+    const imageEl = new Image()
+    imageEl.src = img
+  })
 
   const styledMapType = new google.maps.StyledMapType(styledData);
 
